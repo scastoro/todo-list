1238a03a059eeac2e42ee9332f84f361fb9ac051
Sal Castoro <salcastoro@gmail.com>
2022-01-17 15:30:04 -0500

    Add more functionality to expantedTodo module

--- src/expandedTodo.js
+++ src/expandedTodo.js
@@ -1,6 +1,9 @@
 const expandedTodo = (obj, event) => {
 
   // const currentDiv = event.target
+  const expandedContainer = document.createElement('div');
+  expandedContainer.classList.toggle('todo-list-expanded');
+
   const attrList = document.createElement('ul');
 
   const description = document.createElement('li');
@@ -12,18 +15,42 @@ const expandedTodo = (obj, event) => {
   const notes = document.createElement('li');
   notes.appendChild(document.createTextNode(obj.notes));
 
+  const buttonContainer = document.createElement('div');
+  buttonContainer.classList.toggle('button-container');
+
+  const inputDiv = document.createElement('div');
+  inputDiv.classList.toggle('input-div');
+
   const complete = document.createElement('input');
   complete.type = 'Checkbox';
+  complete.id = 'complete'
+  complete.name = 'complete'
   if(complete) {
     complete.checked = 'yes';
   } 
 
+  const completeLabel = document.createElement('label');
+  completeLabel.for = 'complete';
+  completeLabel.appendChild(document.createTextNode('Todo Complete'));
+
+  const editBtn = document.createElement('button');
+  editBtn.classList.toggle('todo-edit-btn');
+  editBtn.appendChild(document.createTextNode('Edit todo'));
+
+  inputDiv.appendChild(complete);
+  inputDiv.appendChild(completeLabel);
+  buttonContainer.appendChild(inputDiv);
+  buttonContainer.appendChild(editBtn);
+
   attrList.appendChild(description);
   attrList.appendChild(priority);
   attrList.appendChild(notes);
-  attrList.appendChild(complete);
+  
 
-  document.body.appendChild(attrList);
+  expandedContainer.appendChild(attrList);
+  expandedContainer.appendChild(buttonContainer);
+
+  document.body.appendChild(expandedContainer);
 }
 
 const testObj = {
